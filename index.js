a4288a32e809c4f9a90fc33c29897f4adc6ed4d7
Plus Pingya <pluspingya@gmail.com>
2019-12-06 08:21:47 +0700

    fix express to serve react-app as static file properly

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const path = require('path')
 
 const dataStore = {
   accounts: {},
@@ -8,14 +9,18 @@ const dataStore = {
 
 const app = express()
 app.use(bodyParser.json())
-app.use(express.static('public')) //TODO: use Nginx to serve client app
+app.use(express.static(path.join(__dirname, 'client/build')))
 
 require('./endpoints/portfolioEndpoints')(app, dataStore)
 require('./endpoints/balanceEndpoints')(app, dataStore)
 require('./endpoints/tradingEndpoints')(app, dataStore)
 require('./endpoints/quoteEndpoints')(app, dataStore)
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'))
+})
+
 const port = process.env.PORT || 5000
 app.listen(port)
 
-console.log(`Server is running and listening on port ${port}`)
+console.log(`MyStock app is listening on port ${port}`)
